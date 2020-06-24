2599c3296878dc3b10388e605f035559e553d2fb
DanielaHA <dhiguitaa17@gmail.com>
2020-06-24 09:38:30 -0500

    inicio con html, csss y variables

---
+++ space-invaders/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+	// variables
+	const squares = document.querySelectorAll(".grid div");
+	const resultDisplay = document.querySelector("#result");
+	let width = 15;
+	let currentShooterIndex = 202;
+	let currentInvaderIndex = 0;
+	let alienInvaderTakenDown = [];
+	let result = 0;
+	let direction = 1;
+	let invaderId;
+});
